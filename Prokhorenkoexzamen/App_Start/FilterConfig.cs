9ce4b4eb0da07bdf1973de2505d803c0ce240618
Vladimir Prokhorenko <wowa2k@gmail.com>
2017-12-12 18:16:03 +0300

    Add project files.

---
+++ Prokhorenkoexzamen/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Prokhorenkoexzamen
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
