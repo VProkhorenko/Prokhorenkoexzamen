8d430ed8172b487592017681627fba7586f840e7
Vladimir Prokhorenko <wowa2k@gmail.com>
2017-12-13 09:23:24 +0300

    работает

--- Prokhorenkoexzamen/Controllers/HomeController.cs
+++ Prokhorenkoexzamen/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -26,5 +27,37 @@ namespace Prokhorenkoexzamen.Controllers
 
             return View();
         }
+
+        //public ActionResult File()
+        //{
+        //    ViewBag.Message = "Передача файлов на сервер.";
+
+        //    return View();
+        //}
+
+
+       
+        // GET: Home/File
+        public ActionResult File()
+        {
+            return View();
+        }
+
+        // POST: Home/File
+        [HttpPost]
+        public ActionResult File(HttpPostedFileBase fileUpload)
+        {
+            //public string name = "";
+            if (fileUpload == null) return View();
+            else
+            {
+                string path = Server.MapPath("~/Files");
+                string name = Path.GetFileName(fileUpload.FileName);
+                fileUpload.SaveAs(Path.Combine(path, name));
+
+                return RedirectToAction("File");
+            }
+        }
+
     }
 }
